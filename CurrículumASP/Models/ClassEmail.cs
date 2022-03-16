21c297348301d73268428187fc25d4d75aae58ad
Sergi Castillo Tiñena <sergicastillo@hotmail.es>
2022-03-16 19:02:35 +0100

    ASP.net

---
+++ CurrículumASP/Models/ClassEmail.cs
@@ -0,0 +1,9 @@
+namespace CurrículumASP.Models
+{
+    public class ClassEmail
+    {
+        public string asunto { get; set; }
+        public string email { get; set; }
+        public string mensaje { get; set; }
+    }
+}
---
+++ CurrículumASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurrículumASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
